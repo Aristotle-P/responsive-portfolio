ef91b227e0f64b6ff3fe5ccd994cf2f4419f8e01
Sully Poultney <sully@Sullys-MacBook-Air.local>
2019-08-27 19:38:51 -0400

    Updates

---
+++ precache-manifest.eea5daadf2794672b731909c12e5ddb1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f491eaa2da0ea37384d88fb62425428",
+    "url": "/responsive-portfolio/static/media/desktop-hero.7f491eaa.jpeg"
+  },
+  {
+    "revision": "d720863cd5173dec2cae",
+    "url": "/responsive-portfolio/static/css/main.f60180ab.chunk.css"
+  },
+  {
+    "revision": "591664a1c0dde4f94f41",
+    "url": "/responsive-portfolio/static/js/1.591664a1.chunk.js"
+  },
+  {
+    "revision": "2167bff112841ce45099",
+    "url": "/responsive-portfolio/static/js/runtime~main.2167bff1.js"
+  },
+  {
+    "revision": "d203cdabbb03620a798e17e6adc43e17",
+    "url": "/responsive-portfolio/static/media/mobile-hero.d203cdab.jpeg"
+  },
+  {
+    "revision": "d720863cd5173dec2cae",
+    "url": "/responsive-portfolio/static/js/main.d720863c.chunk.js"
+  },
+  {
+    "revision": "d5c0f99a9a6bd25bab26c5488a0c55f9",
+    "url": "/responsive-portfolio/static/media/clientpanel.d5c0f99a.png"
+  },
+  {
+    "revision": "3c6c33bbff592345160a023253c3a792",
+    "url": "/responsive-portfolio/static/media/contactmanager.3c6c33bb.png"
+  },
+  {
+    "revision": "9ac279f97fd30bde7b19709196d0a23c",
+    "url": "/responsive-portfolio/static/media/github.9ac279f9.png"
+  },
+  {
+    "revision": "61ab18f398d3ae46dac70907e5a57d23",
+    "url": "/responsive-portfolio/static/media/microblogger.61ab18f3.png"
+  },
+  {
+    "revision": "dc67ca86ab5f2a7024c0bd22117822c1",
+    "url": "/responsive-portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0e3c94dd9be662be46d56b462bc7fcaa.js"
+  "/responsive-portfolio/precache-manifest.eea5daadf2794672b731909c12e5ddb1.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/responsive-portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
